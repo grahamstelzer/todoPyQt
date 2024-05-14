7afba6d858d4fd9a43f32535c879f7226b12b557
Graham Stelzer <graham.stelzer.01@gmail.com>
2024-05-14 18:41:30 -0400

    Added up/down ctrl

--- todo.py
+++ todo.py
@@ -5,14 +5,9 @@ from PyQt5.QtCore import Qt
 
 
 # =======================================================
-# TODO: Add multilevel ids for each data item
-# initially, data is stored as [status, text] in data.db
-# change to [ general id (string), 
-#             indentation level (initially 1, increase per subtask level),
-#             status, 
-#             text ]
-#
-# implement handling for id and indentation level, start wit buttons change to draggable
+# TODO: 
+# - add draggability
+# - make it look better
 # =======================================================
 
 
@@ -24,7 +19,7 @@ tick = QtGui.QImage('tick.png') # checkmark png
 
 class TodoModel(QtCore.QAbstractListModel):
     def __init__(self, *args, todos=None, **kwargs):
-        super(TodoModel, self).__init__(*args, **kwargs) # inherit from QAbstractListModel (?)
+        super(TodoModel, self).__init__(*args, **kwargs) #s inherit from QAbstractListModel (?)
         self.todos = todos or [] # class variable from todos as an arg or create a new empty
         
     # Required methods for QAbstractListModel
@@ -57,53 +52,81 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
+
         self.setupUi(self)
         self.model = TodoModel()
         self.load()
         self.todoView.setModel(self.model)
         self.addButton.pressed.connect(self.add)
-        self.deleteButton.pressed.connect(self.delete)
-        self.completeButton.pressed.connect(self.complete)
+        # self.deleteButton.pressed.connect(self.delete)
+        # self.completeButton.pressed.connect(self.complete)
 
         # styling
         with open('stylesheet.qss', 'r') as f:
             self.setStyleSheet(f.read())
 
-    # key eventts
+    # key events
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Return:
             self.add()
         if event.key() == Qt.Key_Delete:
             self.delete() 
-        if event.key() == Qt.Key_Space: # doesnt work, maybe need to select item?
-            self.complete()
+        # if event.key() == Qt.Key_Space: # doesnt work, maybe need to select item?
+        #     self.complete()
         if event.key() == Qt.Key_Right:
             self.indent(self.todoView.selectedIndexes()[0]) # selected item is index arg
         if event.key() == Qt.Key_Left:
             self.outdent(self.todoView.selectedIndexes()[0])
 
+        modifiers = QtWidgets.QApplication.keyboardModifiers()
+        if event.key() == QtCore.Qt.Key_W and modifiers == QtCore.Qt.ControlModifier:
+            self.move_up()
+        if event.key() == QtCore.Qt.Key_S and modifiers == QtCore.Qt.ControlModifier:
+            self.move_down()
+
+
+
     # add mouse events, draggable entries:
     def mousePressEvent(self, event):
+        # since indentation handled via arrow keys, draggability will just change rows:
         pass
 
     def indent(self, index):
         # this is dumb but we are just going to add 2 spaces to the beginning of text:
         # unpack
-        indent_level, status, text = self.model.todos[index.row()]
+        indent_level, _, text = self.model.todos[index.row()]
         # add 4 spaces to the beginning of text:
         text = "    " + text
         # add 1 to indent level
-        self.model.todos[index.row()] = (indent_level + 1, False, text)
+        self.model.todos[index.row()] = (indent_level + 1, _, text)
         self.model.layoutChanged.emit()
         self.save()
     
     def outdent(self, index):
         # remove 4 spaces
-        indent_level, status, text = self.model.todos[index.row()]
+        indent_level, _, text = self.model.todos[index.row()]
         text = text[4:]
         if indent_level == 0:
             return
-        self.model.todos[index.row()] = (indent_level - 1, False, text)
+        self.model.todos[index.row()] = (indent_level - 1, _, text)
+        self.model.layoutChanged.emit()
+        self.save()
+
+    # move items up or down:
+    def move_up(self):
+        index = self.todoView.selectedIndexes()[0]
+        row = index.row()
+        if row == 0:
+            return
+        self.model.todos[row], self.model.todos[row - 1] = self.model.todos[row - 1], self.model.todos[row]
+        self.model.layoutChanged.emit()
+        self.save()
+    def move_down(self):
+        index = self.todoView.selectedIndexes()[0]
+        row = index.row()
+        if row == len(self.model.todos) - 1:
+            return
+        self.model.todos[row], self.model.todos[row + 1] = self.model.todos[row + 1], self.model.todos[row]
         self.model.layoutChanged.emit()
         self.save()
 
@@ -140,12 +163,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         if indexes:
             index = indexes[0]
             row = index.row()
-            status, text = self.model.todos[row]
-            self.model.todos[row] = (True, text)
+            _, status, text = self.model.todos[row]
+            self.model.todos[row] = (_, True, text)
+         
             # .dataChanged takes top-left and bottom right, which are equal 
             # for a single selection.
             self.model.dataChanged.emit(index, index)
             # Clear the selection (as it is no longer valid).
+
             self.todoView.clearSelection()
             self.save()
     
